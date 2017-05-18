fffbed1fec2339bc5ee9e2f7f5a9602b14a9da20
Osama Hassanein <player1os@gmail.com>
2017-05-18 16:23:47 +0200

    Enhance the webpack configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,3 +1,7 @@
+// Load the package file.
+import packageConfig from './package.json'
+
+// Expose the configuration object.
 export default {
 	entry: [
 		'babel-polyfill',
@@ -6,15 +10,28 @@ export default {
 	output: {
 		filename: './lib/index.js',
 	},
+	target: 'node',
+	externals: Object.keys(packageConfig.dependencies || {})
+		.reduce((externals, dependencyName) => {
+			externals[dependencyName] = `commonjs ${dependencyName}`
+			return externals
+		}, {}),
+	node: {
+		__filename: true,
+		__dirname: true,
+	},
 	module: {
 		loaders: [{
 			test: /\.ts$/,
 			exclude: /node_modules/,
-			loader: "babel-loader!ts-loader"
+			loader: 'babel-loader!ts-loader'
 		}],
 	},
 	resolve: {
-		extensions: [".ts", ".js"],
+		extensions: [
+			'.ts',
+			'.js',
+		],
 	},
 	devtool: 'source-map',
 }
