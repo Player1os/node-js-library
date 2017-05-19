7a46f8ca6dcd706716c6233e3846b7e6908ccaa5
Osama Hassanein <player1os@gmail.com>
2017-05-19 14:57:57 +0200

    Add comments to webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,38 +8,53 @@ const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.jso
 // Expose the configuration object.
 export default {
 	entry: [
+		// Include the polyfill required by babel into the bundle.
 		'babel-polyfill',
+		// Include the project starting point into the bundle.
 		path.join(__dirname, 'src', 'index.ts'),
 	],
 	output: {
+		// Define the directory for the compilation output.
 		path: path.join(__dirname, 'lib'),
+		// Define the name for the compilation output.
 		filename: 'index.js',
 	},
+	// Specify the target to be the node runtime.
 	target: 'node',
+	// Specify that all standard dependencies should be considered exterals to be dynamically included.
 	externals: Object.keys(packageJson.dependencies || {})
 		.reduce((externals, dependencyName) => {
 			externals[dependencyName] = `commonjs ${dependencyName}`
 			return externals
 		}, {}),
+	// Specify the global declarations of the node runtime.
 	node: {
 		__filename: true,
 		__dirname: true,
 	},
 	module: {
-		loaders: [{
-			test: /\.ts$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader!ts-loader',
-		}],
+		loaders: [
+			// Specify a loader for all typescript source files that first compiles typescript into es2017 standards javascript
+			// using the typescript compiler and then compiles that to javascript supported by the current node runtime
+			// using the babel compiler.
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader!ts-loader',
+			},
+		],
 	},
 	resolve: {
+		// Specify that the '#' character in imports should be resolved to the project's root path.
 		alias: {
 			'#': __dirname,
 		},
+		// Specify that the typescript and javascript extensions can be ommitted from module names.
 		extensions: [
 			'.ts',
 			'.js',
 		],
 	},
+	// Specify that a sourcemap should be created for the outputted bundle.
 	devtool: 'source-map',
 }
