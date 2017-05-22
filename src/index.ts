0a515dd3dc483e009e071479e17ff9673d3ad7b3
Osama Hassanein <player1os@gmail.com>
2017-05-22 14:48:31 +0200

    Remove linting and test issues

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-console.log('Hello')
+console.log('Hello') // tslint:disable-line no-console
--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import {
-	assert,
-} from 'chai'
-
-describe('Test suite', () => {
-	it('Test instance', () => {
-		assert(false, 'No tests are configured')
-	})
-})
